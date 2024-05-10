f368b0e2dd0323b2bc3918bbeb1a9202ffc822cd
Macowen14 <163148525+Macowen14@users.noreply.github.com>
2024-05-10 15:31:23 +0300

    update

--- arsha.js
+++ arsha.js
@@ -1,11 +1,23 @@
-// Show loader when page starts loading
-window.addEventListener("load", function () {
-  var loader = document.getElementById("loader-wrapper");
-  loader.style.display = "none";
-});
+// // Show loader when page starts loading
+// window.addEventListener("load", function () {
+//   var loader = document.getElementById("loader-wrapper");
+//   loader.style.display = "none";
+// });
 
-// Show loader when navigating to another page within the website
-document.addEventListener("DOMContentLoaded", function () {
-  var loader = document.getElementById("loader-wrapper");
-  loader.style.display = "block";
-});
+// // Show loader when navigating to another page within the website
+// document.addEventListener("DOMContentLoaded", function () {
+//   var loader = document.getElementById("loader-wrapper");
+//   loader.style.display = "block";
+// });
+// Function to toggle the icon
+function toggleIcon() {
+  var icon = document.querySelector(".navbar-toggler .toggler-icon i");
+  // Toggle between menu and cancel icons
+  if (icon.classList.contains("fa-bars")) {
+    icon.classList.remove("fa-bars", "fa-solid");
+    icon.classList.add("fa-solid", "fa-xmark-large");
+  } else {
+    icon.classList.remove("fa-solid", "fa-xmark-large");
+    icon.classList.add("fa-bars", "fa-solid");
+  }
+}
