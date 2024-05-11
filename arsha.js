47ca148ea0cbfd1478f51e3e5cbfcaf16b520752
Macowen14 <163148525+Macowen14@users.noreply.github.com>
2024-05-11 15:10:29 +0300

    pricing

--- arsha.js
+++ arsha.js
@@ -1,14 +1,3 @@
-// // Show loader when page starts loading
-// window.addEventListener("load", function () {
-//   var loader = document.getElementById("loader-wrapper");
-//   loader.style.display = "none";
-// });
-
-// // Show loader when navigating to another page within the website
-// document.addEventListener("DOMContentLoaded", function () {
-//   var loader = document.getElementById("loader-wrapper");
-//   loader.style.display = "block";
-// });
 // Function to toggle the icon
 function toggleIcon() {
   var icon = document.querySelector(".navbar-toggler .toggler-icon i");
@@ -21,3 +10,51 @@ function toggleIcon() {
     icon.classList.add("fa-bars", "fa-solid");
   }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const portfolioItems = document.querySelectorAll(".portfolio-item");
+
+  // Function to filter portfolio items based on category
+  function filterItems(category) {
+    portfolioItems.forEach(function (item) {
+      if (category === "*" || item.classList.contains(category)) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    });
+  }
+
+  // Add click event listeners to filter buttons
+  const filterButtons = document.querySelectorAll(".filters li");
+  filterButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      const category = this.getAttribute("data-filter");
+      filterItems(category);
+    });
+  });
+
+  // Add click event listener to close image viewer
+  const closeButtons = document.querySelectorAll(".close-btn");
+  closeButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      const imageViewer = document.querySelector(".image-viewer");
+      imageViewer.style.display = "none";
+    });
+  });
+
+  // Add click event listener to open image viewer
+  const imageLinks = document.querySelectorAll(
+    ".portfolio-info a[data-gallery]"
+  );
+  imageLinks.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const imageUrl = this.getAttribute("href");
+      const imageViewer = document.querySelector(".image-viewer");
+      const imageView = imageViewer.querySelector(".image-view");
+      imageView.setAttribute("src", imageUrl);
+      imageViewer.style.display = "block";
+    });
+  });
+});
